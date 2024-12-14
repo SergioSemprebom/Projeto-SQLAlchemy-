2c1c126cbff5569679d4f63c9919b0732f818c51
Sergio Semprebom <sergiosemprebom@gmail.com>
2024-12-14 19:21:01 -0300

    Projeto SQLAlchemy

--- desafio.py
+++ desafio.py
@@ -1,3 +1,38 @@
-from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base  
 from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class Fornecedores(Base):
+
+    __tablename__ = 'Fornecedores'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(50), nullable=False)
+    telefone = Column(String,(20))
+    email = Column(String(50))
+    endereco = Column(String(100))
+
+    def __init__(self, nome, telefone, email, endereco):
+        self.nome = nome
+        self.telefone = telefone
+        self.email = email
+        self.endereco = endereco
+
+class Produto(Base):
+
+    __tablename__ = 'produtos'
+    id = (Column(Integer, primary_key=True))
+    nome = Column(String(50, nullable=False))
+    descricao = Column(String(200))
+    preco = Column(Integer)
+    fornecedor = Column(Integer, ForeignKey ('fornecedores'))
+
+    # Estabelece a relação entre produto e fornecedor
+    fornecedor = relationship ("Fornecedor")
+
+    engine = create_engine('sqlite:///:memory:',cecho=True)
+    Base.metadata.create.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
